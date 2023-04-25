e0132796e391018fbfe887461451456df267ddd2
Assylbek Zhumasserikov <assylbek.zhumasserikov@gmail.com>
2023-04-25 12:34:51 +0600

    Initial Commit

---
+++ api/views.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
+import xmltodict
+import json
+
+
+def convert_to_json(data):
+    convert = xmltodict.parse(data.text)
+    result = json.dumps(convert)
+    return json.loads(result)
+
+
+@api_view(["POST"])
+def vuln_results(request):
+    url = "https://qualysapi.qg2.apps.qualys.eu/api/2.0/fo/asset/host/vm/detection"
+    headers = {"X-Requested-With": "Curl"}
+    auth = (request.data.get("username"), request.data.get("password"))
+    params = {"action": "list"}
+    xml_result = requests.get(url=url, params=params, headers=headers, auth=auth)
+    return Response(convert_to_json(xml_result))
